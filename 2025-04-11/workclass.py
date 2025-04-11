a1f4bee181265a9c56be5155e641bf7e1efaffc1
Anton Makridin <makridin@gmail.com>
2025-04-11 21:57:23 +0500

    работа в классе 11.04.2025

---
+++ 2025-04-11/workclass.py
@@ -0,0 +1,18 @@
+# import random
+# rnd = random.randint(2, 9)
+# i = 0
+# while i<5:
+#     vvod = int(input('Введи число от 2 до 9: '))
+#     if vvod == rnd:
+#         print('Угадал')
+#         break    
+#     print(f'Не угадал, я загадал число {rnd}')
+#     i+=1
+# else:
+#     print('Угадал!')
+
+
+name = 'КолОбОков'
+
+name = name.lower()
+print(name.count('о'))

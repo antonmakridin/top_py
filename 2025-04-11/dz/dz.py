544c00f9aaba0029843e9d08f79de52f3c7c9244
Anton Makridin <makridin@gmail.com>
2025-04-18 19:30:07 +0500

    dz

--- 2025-04-11/dz/dz.py
+++ 2025-04-11/dz/dz.py
@@ -2,7 +2,13 @@
 # passwd = input('Введите пароль: ')
 # must = '$'
 # min_sym = 8
+# вариант 1
 # if len(passwd) >= min_sym and must in passwd:
+#     print('Пароль подходит')
+# else:
+#     print('Пароль не подходит')
+# вариант 2
+# if len(passwd) >= 8 and passwd.find(must) > 1:
 #     print('Пароль подходит')
 # else:
 #     print('Пароль не подходит')
@@ -19,18 +25,25 @@
 
 
 # задание 3 индекс входждения символа
-# text = input("Введите какие-то данные: ")
-# sym = input("Введите символ: ")
+text = input("Введите какие-то данные: ")
+sym = input("Введите символ: ")
 # if len(sym) != 1:
 #     print("Пожалуйста, введите только один символ.")
 # else:
+# вариант 1
 #     if sym in text:
 #         index = text.index(sym)
 #         print(f"Символ найден. Индекс первого вхождения: {index}")
 #     else:
 #         print("Символ отсутствует.")
+# вариант 2
+    # if text.find(sym) > 1:
+    #     index = text.index(sym)
+    #     print(f"Символ найден. Индекс первого вхождения: {index}")
+    # else:
+    #     print("Символ отсутствует.")
 
 
 # задание 4 замена пробелов
-text = input("Введите какие-то данные: ")
-print(f'Измененный текст {text.replace(" ", "1")}')
+#text = input("Введите какие-то данные: ")
+#print(f'Измененный текст {text.replace(" ", "1")}')

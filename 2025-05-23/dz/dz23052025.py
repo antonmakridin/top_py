25b9b14a102b41992d983929dc41bd86bf2f5012
Anton Makridin <makridin@gmail.com>
2025-05-26 11:32:48 +0500

    dz23032025

---
+++ 2025-05-23/dz/dz23052025.py
@@ -0,0 +1,29 @@
+# #1
+# data = [[32, 34, 12, 1], [1, 4, 3], [12, 4, 3, 2]]
+# res = []
+# for d in data:
+#     m = d[0]
+#     for i in d:
+#         if i > m:
+#             m = i
+#     res.append(m)
+# print(res)
+
+
+# #2
+# words = ['apple', 'banana', 'cherry']
+# # stroka = ''
+# # for s in words:
+# #     stroka += s
+# # print(len(stroka))
+# #или
+# print(len(''.join(words)))
+
+
+#3
+words = ['apple', 'banana', 'cherry', 'cherry', 'lemon', 'apple']
+unique_words = []
+for w in words:
+    unique_words.append(w)
+    print(w, '-', words.count(w))
+    words.remove(w)

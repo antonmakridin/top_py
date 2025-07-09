b78ed022b196196884bee2b02fc4b88a63c83bad
Anton Makridin <makridin@gmail.com>
2025-07-09 16:07:49 +0500

    dz39-40(telegram bot)

--- 2025-07-08/class2.py
+++ 2025-07-08/class2.py
@@ -2,20 +2,19 @@ import time
 import requests
 import random
 
+start_num = 1
+end_num = 5
+game_status = {} #храним данные об играх каждого пользователя
+user_admin = 496775340
+
 TOKEN = "7305551623:AAH0quKy8Rc5zVkl2FkXlD3G75C0xurWvi0"
-URL = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
+
+id_last_message = ''
 
 def get_updates():
-    response = requests.get(URL)
+    url = f"https://api.telegram.org/bot{TOKEN}/getUpdates?offset=-1"
+    response = requests.get(url)
     return response.json()
-
-# updates = get_updates()
-# if updates['result']:
-#     for item in updates['result']:
-#         ts = item['message']['date']
-#         local = time.localtime(time.time())
-#         ts = time.asctime(local)
-#         print(f"Сообщение от пользователя: {item['message']['from']['first_name']} (id: {item['message']['from']['id']}), текст сообщения: {item['message']['text']}, время: {ts}")
 
 
 def send_message(chat_id, text):
@@ -24,39 +23,58 @@ def send_message(chat_id, text):
     response = requests.get(url, params=params)
     return response.json()
 
+def start_game(user_id):
+    # запускаем игру
+    game_status[user_id] = {'right_answer': random.randint(start_num, end_num), 'attempts': 3, 'in_game': True}
+    message = 'Угадай число от ' + str(start_num) + ' до ' + str(end_num) + '. У тебя 3 попытки'
+    send_message(user_id, message)
+    
+def process_answer(user_id, answer):
+    #чекаем введенные пользователем символы
+    if not answer.isdigit():
+        send_message(user_id, 'Вы ввели не только цифры')
+        return
+    
+    answers = int(answer)
+    if  not start_num <= answers <= end_num:
+        message = 'Число должно быть от ' + str(start_num) + ' до ' + str(end_num) + '.'
+        send_message(user_id, message)
+    
+    game_data = game_status[user_id]
 
+    if answers == game_data['right_answer']:
+        send_message(user_id, 'Молодец! Угадал!')
+        game_data['in_game'] = False
+        return
+    
+    game_data['attempts'] -= 1
 
-def play_game(user_id, text):
-    e = 3
-    while e > 0:
-        send_message(user_id, text)
-        
-
-rand_num = random.randint(1, 3)
-print(rand_num)
-print('Угадай число от 1 до 3')
-for i in range(3):
-    num = int(input('Число: '))
-    if num == rand_num:
-        print('Угадал')
-        break
-
-
-# задаем вопрос загадай
-# после ответа 1 должен еще раз угадай
-# после ответа 2 еще раз угадай
-# после ответа 3  вывести результат
+    if game_data['attempts'] > 0:
+        message = 'Не угадал:(. Осталось попыток: ' + str(game_data['attempts']) + '. правильный ответ - ' + str(game_data['right_answer'])
+        send_message(user_id, message)
+    else:
+        message = 'Игра окончена. Я загадал число - ' + str(game_data['right_answer'])
+        send_message(user_id, message)
+        game_data['in_game'] = False
 
 def check_updates():
+    updates = get_updates()
+    if updates['result']:
+        id_last_message = updates['result'][0]['update_id']
     while True:
         updates = get_updates()
-        t = time.time()
         if updates['result']:
             for item in updates['result']:
-                if int(t) < item['message']['date'] + 1:
-                    if item['message']['text'] == 'сыграем':
-                        play_game(item['message']['from']['id'], 'угадай число от 1 до 3')
-                    # print(f"Сообщение от пользователя: {item['message']['from']['first_name']}, текст сообщения: {item['message']['text']} время: {item['message']['date']} {int(t)}")
-        time.sleep(1)  # Задержка в 1 секунду
+                if int(item['update_id']) > id_last_message:
+                    user_id = item['message']['from']['id']
+                    text = item['message']['text']
+                    # print(f"Сообщение от пользователя: {user_id}, текст: {text}")
+                    if text.lower() == 'сыграем':
+                        start_game(user_id)
+                    elif user_id in game_status and game_status[user_id]['in_game']:
+                        process_answer(user_id, text)
+                    id_last_message = int(item['update_id'])
+        time.sleep(1)
+send_message(user_admin, 'бот запущен')
 check_updates()
 

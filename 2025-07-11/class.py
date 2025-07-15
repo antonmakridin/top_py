82d6ab6a29c43da4836ff6a93feab655f81fc9de
Anton Makridin <makridin@gmail.com>
2025-07-15 19:05:50 +0500

    dz41-40

--- 2025-07-11/class.py
+++ 2025-07-11/class.py
@@ -5,16 +5,20 @@ import json
 
 start_num = 1
 end_num = 5
-game_status = {} #храним данные об играх каждого пользователя
+game_status = {} # храним данные об играх каждого пользователя
 user_admin = 496775340
+variant_answer = ["Норм", "Хорошо", "Отлично!"]
 
 TOKEN = "7305551623:AAH0quKy8Rc5zVkl2FkXlD3G75C0xurWvi0"
 
 id_last_message = ''
 
-def get_updates():
-    url = f"https://api.telegram.org/bot{TOKEN}/getUpdates?offset=-1"
-    response = requests.get(url)
+def get_updates(offset=None):
+    url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
+    params = {'timeout': 10}
+    if offset:
+        params['offset'] = offset
+    response = requests.get(url, params=params)
     return response.json()
 
 def send_message(chat_id, text):
@@ -23,42 +27,21 @@ def send_message(chat_id, text):
     response = requests.get(url, params=params)
     return response.json()
 
-def send_message_html(chat_id, text, parse_mode):
-    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
-    params = {'chat_id': chat_id, 'text': text, 'parse_mode': parse_mode}
-    response = requests.get(url, params=params)
-    return response.json()
-
-def send_message_button(chat_id, text):
+def send_game_button(chat_id, text):
     url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
     params = {
-    'chat_id': chat_id,
-    'text': text,
-    'reply_markup': {
-        'inline_keyboard': [
-        [
-            {
-            'text': "Кнопка 1",
-            'url': 'https://msp.midural.ru'
-            },
-            {
-            'text': 'Кнопка 2',
-            'callback_data': 'hfjdksfhsd'
-            }
-        ]
-        ]
+        'chat_id': chat_id,
+        'text': text,
+        'reply_markup': {
+            'keyboard': [
+                [{'text': 'Сыграем'}]
+            ],
+            'resize_keyboard': True,
+            'one_time_keyboard': True
+        }
     }
-    }
-    headers = {
-    'Content-Type': 'application/json'
-    }
-    response = requests.request('POST', url, headers=headers, data=json.dumps(params))
-    print(response.text)
-
-def inline_keyboard(chat_id, text, parse_mode):
-    url = f'https://api.telegram.org/bot{TOKEN}/inline_keyboard'
-    params = {'chat_id': chat_id, 'text': text, 'parse_mode': parse_mode}
-    response = requests.get(url, params=params)
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(url, headers=headers, data=json.dumps(params))
     return response.json()
 
 def start_game(user_id):
@@ -68,15 +51,16 @@ def start_game(user_id):
     send_message(user_id, message)
     
 def process_answer(user_id, answer):
-    #чекаем введенные пользователем символы
+    # чекаем введенные пользователем символы
     if not answer.isdigit():
         send_message(user_id, 'Вы ввели не только цифры')
         return
     
     answers = int(answer)
-    if  not start_num <= answers <= end_num:
+    if not start_num <= answers <= end_num:
         message = 'Число должно быть от ' + str(start_num) + ' до ' + str(end_num) + '.'
         send_message(user_id, message)
+        return
     
     game_data = game_status[user_id]
 
@@ -88,7 +72,7 @@ def process_answer(user_id, answer):
     game_data['attempts'] -= 1
 
     if game_data['attempts'] > 0:
-        message = 'Не угадал:(. Осталось попыток: ' + str(game_data['attempts']) + '. правильный ответ - ' + str(game_data['right_answer'])
+        message = 'Не угадал:(. Осталось попыток: ' + str(game_data['attempts']) + '.'
         send_message(user_id, message)
     else:
         message = 'Игра окончена. Я загадал число - ' + str(game_data['right_answer'])
@@ -96,25 +80,30 @@ def process_answer(user_id, answer):
         game_data['in_game'] = False
 
 def check_updates():
-    updates = get_updates()
-    if updates['result']:
-        id_last_message = updates['result'][0]['update_id']
+    last_update_id = None
     while True:
-        updates = get_updates()
-        if updates['result']:
+        updates = get_updates(offset=last_update_id)
+        if updates.get('result'):
             for item in updates['result']:
-                # if int(item['update_id']) > id_last_message:
-                    # user_id = item['message']['from']['id']
-                    # text = item['message']['text']
-                    # # print(f"Сообщение от пользователя: {user_id}, текст: {text}")
-                    # if text.lower() == 'сыграем':
-                    #     start_game(user_id)
-                    # elif user_id in game_status and game_status[user_id]['in_game']:
-                    #     process_answer(user_id, text)
-                    # id_last_message = int(item['update_id'])
-                print(updates)
-        time.sleep(1)
-# send_message_html(user_admin, '<b>Бот</b> запущен', 'HTML')
-send_message_button(user_admin, '<b>Бот</b> запущен')
-check_updates()
+                last_update_id = item['update_id'] + 1
+                
+                if 'message' in item:
+                    message = item['message']
+                    user_id = message['from']['id']
+                    text = message.get('text', '')
+                    
+                    if text == 'Сыграем':
+                        start_game(user_id)
+                    elif text.lower() == '/start':
+                        send_game_button(user_id, 'Нажми кнопку "Сыграем", чтобы начать игру!')
+                    elif user_id in game_status and game_status[user_id]['in_game']:
+                        process_answer(user_id, text)
+                    elif 'как дела' in text:
+                        send_message(user_id, random.choice(variant_answer))
+                    elif 'загадай число' in text:
+                        send_message(user_id, f"Вот твое число: {random.randint(1, 10)}")
 
+        time.sleep(1)
+
+send_game_button(user_admin, 'Бот запущен. Нажми "Сыграем", чтобы начать игру!')
+check_updates()
